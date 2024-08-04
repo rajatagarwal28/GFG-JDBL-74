4472f07b3c7e6059ac4a46960930a295215f6819
rajat agarwal <contact2rajatagarwal@gmail.com>
2024-08-04 10:04:33 +0530

    Collections discussed

---
+++ Collections/CollectionDemo.java
@@ -0,0 +1,44 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import java.util.Set;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.LinkedHashSet;
+
+import java.util.Queue;
+
+import java.util.PriorityQueue;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+import java.util.Map;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+
+
+public class CollectionDemo {
+
+
+    public static void main(String[] args) {
+        List<String> arrayListlist = new ArrayList<String>(); // resizable array
+        List<String> linkedList = new LinkedList<String>(); // resizable array
+
+        arrayListlist.add();
+        arrayListlist.remove();
+        arrayListlist.addAll();
+        arrayListlist.iterator();
+        arrayListlist.forEach();
+
+
+
+    }
+
+}
